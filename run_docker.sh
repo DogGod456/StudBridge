4e66fad3ecee4a0bab1159c438413d6f525c23d0
rus-net <rus-net@mail.ru>
2025-04-28 14:07:14 +0300

    chore: докер разворачивается. Нужен код таблиц

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-IMAGE="networking-application"
-CONTAINER="pong_micro"
+# IMAGE="networking-application"
+# CONTAINER="pong_micro"
 
-echo "start building image..."
-docker build -t $IMAGE .
+# echo "start building image..."
+# docker build -t $IMAGE .
 
-echo "Run container..."
-docker run -d -p 8080:8080 -p 8081:8081 --name $CONTAINER $IMAGE 
+# echo "Run container..."
+# docker run -d -p 8080:8080 -p 8081:8081 --name $CONTAINER $IMAGE 
+
+docker compose up --build 
